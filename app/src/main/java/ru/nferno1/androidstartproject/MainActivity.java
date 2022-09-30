3b86c1a035a41c4fcc9746886d7e7d3962134a23
nferno1 <nferno1@mail.ru>
2022-09-30 07:54:37 +0700

    Added Java screen configuration example

--- app/src/main/java/ru/nferno1/androidstartproject/MainActivity.java
+++ app/src/main/java/ru/nferno1/androidstartproject/MainActivity.java
@@ -3,12 +3,21 @@ package ru.nferno1.androidstartproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        TextView textView = new TextView(this);
+        textView.setText("Hello Android!");
+        textView.setTextSize(22);
+
+        setContentView(textView);
+
+
     }
 }
